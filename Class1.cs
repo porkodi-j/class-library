e9f20fc246b6b19866f0ba56ad39d516098de39a
porkodi-j <j.porkodi@gmail.com>
2023-12-19 11:05:28 +0000

    commit with Readme

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+    }
+}
